60a4867a782b40564544c9de44f1e8b3195adabd
maki.xie <makixie1@outlook.com>
2025-04-13 14:10:15 -0400

    complete the lazy init pattern

--- lazyinit.go
+++ lazyinit.go
@@ -1,5 +1,70 @@
 package gogymnastics
 
-// First Reading:
-// get a more solid understanding of map in golang https://go.dev/blog/maps
-// understand RWMutex
+import (
+	"fmt"
+	"sync"
+)
+
+// Pattern (1) write once at starting time, and
+// read only during the lifetime of the lifetime of the server
+// in this scenario, we can use a map to store the address of the nodes
+
+// Question: if a readonly map safe for concurrent use?
+// Answer: yes, a read-only map is safe for concurrent use.
+var fixedAddress map[string]string
+var fixedMutex map[string]*sync.Mutex
+
+// Pattern (2) the connections have read and writes/updates for dead connection
+// during the runtime of the server, so we need sync map
+var connections *sync.Map
+
+func init() {
+	// set this fixedAddress only once
+	fixedAddress = map[string]string{
+		"node1": "localhost:8080",
+		"node2": "localhost:8081",
+		"node3": "localhost:8082",
+		"node4": "localhost:8083",
+		"node5": "localhost:8084",
+	}
+	fixedMutex = make(map[string]*sync.Mutex)
+	for k := range fixedAddress {
+		fixedMutex[k] = &sync.Mutex{}
+	}
+	// initialize the connections
+	connections = &sync.Map{}
+}
+
+func getAddress(nodeName string) string {
+	return fixedAddress[nodeName]
+}
+
+func mockingEstablishConn(address string) (string, error) {
+	fmt.Printf("Establishing connection to %s\n", address)
+	return "the Connection to " + address, nil
+}
+
+func GetConnection(nodeName string) (string, error) {
+	conn, ok := connections.Load(nodeName)
+	if ok {
+		return conn.(string), nil
+	}
+	// only one goroutine can establish a connection
+	fixedMutex[nodeName].Lock()
+	defer fixedMutex[nodeName].Unlock()
+
+	// check again if the connection is already established
+	conn, ok = connections.Load(nodeName)
+	if ok {
+		return conn.(string), nil
+	}
+	// establish the connection
+	address := getAddress(nodeName)
+	conn, err := mockingEstablishConn(address)
+	if err != nil {
+		return "", err
+	}
+	// store the connection in the map
+	connections.Store(nodeName, conn)
+	return conn.(string), nil
+}
---
+++ lazyinit_test.go
@@ -0,0 +1,49 @@
+package gogymnastics
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestGetConnectionTwice(t *testing.T) {
+
+	fmt.Println("---1st TESTING---")
+	nodeID := "node2"
+
+	// Test the connection establishment
+	conn, err := GetConnection(nodeID)
+	if err != nil {
+		t.Fatalf("Failed to establish connection: %v", err)
+	}
+	if conn != "the Connection to localhost:8081" {
+		t.Fatalf("Expected 'the Connection to localhost:8081', got '%s'", conn)
+	}
+
+	// Test the GetConnection function
+	conn, err = GetConnection(nodeID)
+	if err != nil {
+		t.Fatalf("Failed to get connection: %v", err)
+	}
+	if conn != "the Connection to localhost:8081" {
+		t.Fatalf("Expected 'the Connection to localhost:8081', got '%s'", conn)
+	}
+	// How to check the EstablishConn is called for only once?
+}
+
+func TestParallelGetConnection(t *testing.T) {
+	fmt.Println("---2nd TESTING---")
+	nodeID := "node3"
+	// Test concurrent access to GetConnection
+	var wg sync.WaitGroup
+	getConn := func(threadID int) {
+		defer wg.Done()
+		GetConnection(nodeID)
+		fmt.Printf("thread %d: get the connection to %s\n", threadID, nodeID)
+	}
+	for i := range 10 {
+		wg.Add(1)
+		go getConn(i)
+	}
+	wg.Wait()
+}
