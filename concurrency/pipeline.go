e473e1c2cfe7105deb3334807cec900cbde23a33
maki.xie <makixie1@outlook.com>
2025-07-11 22:28:43 -0400

    add pipeline pattern

--- concurrency/pipeline.go
+++ concurrency/pipeline.go
@@ -2,26 +2,26 @@ package concurrency
 
 import "context"
 
-var stage func(ctx context.Context, in <-chan int) <-chan int
-
+// maki:
+// I think in the pipeline pattern,
+// closing the channel is a good practice
 
 func add(ctx context.Context, in <-chan int) <-chan int {
 	out := make(chan int)
 	go func() {
 		defer close(out)
+		// maki:
+		// but if the in channel is blocked and forgets to close it, the goroutine will leak
+		// ctx cancel cannot help it
+		// so it seems closing a channel is a good practice
 		for v := range in {
-			out <- v + 1
-		}
-	}()
-	return out
-}
-
-func sub(ctx context.Context, in <-chan int) <-chan int {
-	out := make(chan int)
-	go func() {
-		defer close(out)
-		for v := range in {
-			out <- v - 1
+			select {
+			// maki:
+			// the key line is here to prevent possible goroutine leek
+			case <-ctx.Done():
+				return
+			case out <- v + 1:
+			}
 		}
 	}()
 	return out
@@ -32,8 +32,17 @@ func mul(ctx context.Context, in <-chan int) <-chan int {
 	go func() {
 		defer close(out)
 		for v := range in {
-			out <- v * 2
+			select {
+			// maki: the key line is here to prevent possible goroutine leek, but what if the for range blocks
+			case <-ctx.Done():
+				return
+			case out <- v * 2:
+			}
 		}
 	}()
 	return out
+}
+
+func pipeline(ctx context.Context, in <-chan int) <-chan int {
+	return mul(ctx, add(ctx, in))
 }
--- concurrency/pipeline_test.go
+++ concurrency/pipeline_test.go
@@ -14,8 +14,24 @@ func TestPipeline(t *testing.T) {
 			inbound <- i
 		}
 	}()
-	pipeline := sub(context.Background(), mul(context.Background(), add(context.Background(), inbound)))
-	for v := range pipeline {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	for v := range pipeline(ctx, inbound) {
+		fmt.Println(v)
+	}
+}
+
+func TestPipelineWithCancel(t *testing.T) {
+	inbound := make(chan int)
+	go func() {
+		defer close(inbound)
+		for i := range 5 {
+			inbound <- i
+		}
+	}()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	for v := range pipeline(ctx, inbound) {
 		fmt.Println(v)
 	}
 }
