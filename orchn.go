77f58a324165ea6d344509369c03ee416e2dab4f
maki.xie <makixie1@outlook.com>
2025-07-11 20:34:42 -0400

    add orch pattern

---
+++ orchn.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+)
+
+func OrChn(ctx context.Context, chans ...<-chan any) <-chan any {
+
+	if len(chans) == 0 {
+		return nil
+	}
+	if len(chans) == 1 {
+		return chans[0]
+	}
+	orChan := make(chan any)
+
+	go func() {
+		defer close(orChan)
+		recurChan := OrChn(ctx, chans[1:]...)
+		select {
+		case <-ctx.Done():
+			return
+		case value, ok := <-chans[0]:
+			if !ok {
+				return
+			}
+			orChan <- value
+			return
+		case value, ok := <-recurChan:
+			if !ok {
+				return
+			}
+			orChan <- value
+			return
+		}
+	}()
+	return orChan
+}
