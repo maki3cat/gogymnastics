6f6b235693af3f7d614e330e82636bc685408406
maki.xie <makixie1@outlook.com>
2025-07-16 18:05:20 -0400

    add nonblocking send/receive example

---
+++ concurrency_small/nonblocking_test.go
@@ -0,0 +1,30 @@
+package concurrency_small
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestNonblocking_Pass(t *testing.T) {
+	ch := make(chan struct{}, 0)
+	dosomething_once_nonblocking(ch)
+}
+
+func TestNonblocking_Done(t *testing.T) {
+	ch := make(chan struct{}, 1)
+	dosomething_once_nonblocking(ch)
+}
+
+func TestHeartbeat(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		time.Sleep(time.Second * 3)
+		cancel()
+	}()
+	heartbeatChan := startHeartbeat(ctx, 1, time.Second)
+	// maki: for and single select channel should be replaced with heartbeatChan
+	for range heartbeatChan {
+		t.Log("received heartbeat")
+	}
+}
